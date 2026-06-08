4ceff1d7801cb8556bc32c0764054c9ea250d293
itsshinra <voeurnsovanmakara313@gmail.com>
2026-06-08 16:05:15 +0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by sovanmakara on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HotProspectsTests/HotProspectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsTests.swift
+//  HotProspectsTests
+//
+//  Created by sovanmakara on 8/6/26.
+//
+
+import Testing
+@testable import HotProspects
+
+struct HotProspectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
